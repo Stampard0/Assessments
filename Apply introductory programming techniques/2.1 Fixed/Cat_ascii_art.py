ec5707763ed086a8387bc12419d64695c0fef187
Stampard0 <98725092+Stampard0@users.noreply.github.com>
2022-03-01 16:18:43 +0800

    fixed AT2.1 & 2.2

---
+++ Apply introductory programming techniques/2.1 Fixed/Cat_ascii_art.py
@@ -0,0 +1,15 @@
+print("""
+      /\_/\ 
+ /\  / o o \ 
+//\\ \~(*)~/ 
+`  \/   ^ / 
+   | \|| ||  
+   \ '|| ||  
+    \)()-()) 
+""") #using 3 speechmarks makes it so you can do multi line speech.
+print("""
+ /\_/\ 
+( o o ) 
+==_Y_== 
+  `-' 
+""")
