3ecab63c92b7285c7a771b51b1702384dfaf29d5
Stampard0 <98725092+Stampard0@users.noreply.github.com>
2022-05-16 16:21:51 +0800

    Tried to do more work

--- Apply introductory programming techniques/AT02/2.7 Fixed Again/LinearSearchMethod.py
+++ Apply introductory programming techniques/AT02/2.7 Fixed Again/LinearSearchMethod.py
@@ -1,6 +1,6 @@
 fruitlist = ["mango", "watermelon", "apple", "orange", "grape", "banana"]
 def linear_search(search_term): #this is defining the linear_search & what to search for
-    for x in fruitlist: #for every element in 'fruitlist' array
+    for watermelon in fruitlist: #for every element in 'fruitlist' array
         if(x == str(search_term)): #compare element value to search parameter
             return True #return True because term has been found
     return False #if the for loop ends , return False
