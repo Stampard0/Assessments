d5acc037a157e5e33ddce7e392d045a07fdc6538
Stampard0 <98725092+Stampard0@users.noreply.github.com>
2022-05-28 18:56:58 +0800

    Submitted AT02.7

--- Apply introductory programming techniques/AT02/2.7 Fixed Again/LinearSearchMethod_Fixed.py
+++ Apply introductory programming techniques/AT02/2.7 Fixed Again/LinearSearchMethod_Fixed.py
@@ -3,9 +3,7 @@ def linear_search(search_term, index = -1): #this is defining the linear_search 
     #(Setting a default parrameter makes it not return a error)
     for x in fruitlist: #for every element in 'fruitlist' array
         if(x == str(search_term)): #compare element value to search parameter
-            print(search_term, fruitlist.index(search_term)) #using index() makes it return the index of what you search
-        elif(x != str(search_term)): #this only will happen if the search_term is not in fruitlist
-            print(search_term, index) #I am only making it print -1 just so it doesn't bring back a error
+            return (search_term, fruitlist.index(search_term)) #using index() makes it return the index of what you search.
 
-linear_search("watermelon")
-linear_search("pineapple")
+print(linear_search("watermelon"))
+print(linear_search("pineapple"))
