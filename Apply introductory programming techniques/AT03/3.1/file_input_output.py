c1d1521e824eb08ed5b6a0720dbab855a1ff14fb
Stampard0 <98725092+Stampard0@users.noreply.github.com>
2022-06-03 16:04:27 +0800

    Need to combine the GDD parts done pseudo-code for 3.1

--- Apply introductory programming techniques/AT03/3.1/file_input_output.py
+++ Apply introductory programming techniques/AT03/3.1/file_input_output.py
@@ -1,9 +1,9 @@
-f = open("Python.txt", "w") #the w stands for write. using write over rights what was already there
-f.write("""Python has been an important part of Google since the beginning, and remains so as the system grows and evolves.
+output_string = open("Python.txt", "w") #the w stands for write. using write over rights what was already there
+output_string.write("""Python has been an important part of Google since the beginning, and remains so as the system grows and evolves.
 
 "Today dozens of Google engineers use Python, and we're looking for more people with skills in this language." said Peter Norvig, director of search quality at Google, Inc.""")
-f.close()#it is good practice to close the file
+output_string.close()#it is good practice to close the file
 
-f = open("Python.txt", "r") #the r stands for read
-print(f.read()) #this prints what it has read in the txt file
-f.close() #it is good practice to close the file
+input_string = open("Python.txt", "r") #the r stands for read
+print(input_string.read()) #this prints what it has read in the txt file
+input_string.close() #it is good practice to close the file
